9e38ec047ed4765d3afe42c1ad38acd23308c221
Alvaro <v170zam@gmail.com>
2025-07-18 20:45:04 -0400

    feat: first commit push

---
+++ src/app/men/page.tsx
@@ -0,0 +1,10 @@
+import styles from './men.module.css';
+
+export default function MenPage() {
+  return (
+    <main className={styles.mainMen}>
+      <h1 className={styles.titleMen}>Men&apos;s Clothing</h1>
+      <p>Shop the latest men&apos;s fashion.</p>
+    </main>
+  );
+}
---
+++ src/app/women/page.tsx
@@ -0,0 +1,10 @@
+import styles from './women.module.css';
+
+export default function WomenPage() {
+  return (
+    <main className={styles.mainWomen}>
+      <h1 className={styles.titleWomen}>Women&apos;s Clothing</h1>
+      <p>Discover the newest trends in women&apos;s fashion.</p>
+    </main>
+  );
+}
