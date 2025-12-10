7af59e8edc941fb592070292ada289e5621ad71f
Alvaro <v170zam@gmail.com>
2025-12-09 23:11:30 -0400

    feat: add microsoft clarity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,6 +35,18 @@ export default function RootLayout({
           type="image/png"
         />
         <link rel="apple-touch-icon" href="/logos/Logo-Monero.png" />
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "uj4u9y5mht");
+            `,
+          }}
+        />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
