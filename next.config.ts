365ad8be52176e786ba4cd82dec99f8285b385b6
Alvaro <v170zam@gmail.com>
2025-12-17 00:07:55 -0400

    fix: redirection logic

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,21 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'moneroget.com',
+          },
+        ],
+        destination: 'https://www.moneroget.com/:path*',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
